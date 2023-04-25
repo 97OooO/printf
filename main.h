49f478dfcd52a3db34fd3cc56a65b1967d340d35
LAKRIM <lakrimoussama@gmail.com>
2023-04-25 05:21:56 -0700

    main file

--- main.h
+++ main.h
@@ -79,6 +79,15 @@ int my_print_rot13string(va_list types, char buffer[], int flags, int width, int
 
 /* width handler */
 int my_handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
+int my_write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
+int my_write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
+int my_write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
+int my_write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
 
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
-
+#endif
