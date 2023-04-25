4d9592cebc3b5c46e2f463980c6fbd3e35697150
LAKRIM <lakrimoussama@gmail.com>
2023-04-25 08:39:24 -0700

    Functions Project

--- functions.c
+++ functions.c
@@ -164,6 +164,19 @@ int print_binary(va_list types, char buffer[], int flags, int width, int precisi
 	UNUSED(size);
 
 	n = va_arg(types, unsigned int);
-	m = 
+	m = 2147483648;
+	a[0] = n / m;
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i ==31)
+		{
+			char z = '0' + a[i];
+
+			write(l, &z, l);
+			count++;
+		}
+	}
+	return (count);
 
 }
